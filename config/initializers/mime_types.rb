78b1e1f762f679ac8339add5557016a750df4b71
Paul Spieker <p.spieker@duenos.de>
2017-05-10 21:18:11 +0200

    [SETUP] Mime types for json-api

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,4 +1,2 @@
-# Be sure to restart your server when you modify this file.
-
-# Add new mime types for use in respond_to blocks:
-# Mime::Type.register "text/richtext", :rtf
+Mime::Type.unregister :json
+Mime::Type.register 'application/vnd.api+json', :json
